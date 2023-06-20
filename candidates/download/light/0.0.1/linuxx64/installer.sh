79f9269382aa6ba835656565c38612a3cfb0ebce
Arun Suresh <58501088+asa1997@users.noreply.github.com>
2023-06-20 14:15:32 +0530

    Update installer.sh

--- candidates/download/light/0.0.1/linuxx64/installer.sh
+++ candidates/download/light/0.0.1/linuxx64/installer.sh
@@ -27,6 +27,8 @@ if [[ ! -d $HOME/.besman ]]; then
 	echo "Installing BeSman"
 	curl -L https://raw.githubusercontent.com/Be-Secure/BeSman/dist/dist/get.besman.io | bash
 	source $HOME/.besman/bin/besman-init.sh
+else
+	echo "BeSman found"
 fi
 
 if ! echo "$BESMAN_ENV_REPOS" | grep -q "Be-Secure/BeSLab"
