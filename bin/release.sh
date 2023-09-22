bb8c8c28b93ae13eff980cc01b71b72178890fed
asa1997 <arunsureshampadath@gmail.com>
2023-09-22 12:23:29 +0530

    Updated

--- bin/release.sh
+++ bin/release.sh
@@ -18,38 +18,50 @@ if [[ -z $BLIMAN_NAMESPACE ]];
 fi
 
 # prepare branch
-cd $HOME/BLIman
+cd $HOME/BLIman || return 1
 git checkout issue#4
 #git checkout dev
 git branch -D $branch
 git checkout -b $branch
 
+file="$HOME/BLIman/scripts/get.bliman.io.tmpl"
 
 #copy the tmpl file to /scripts
-cp $HOME/BLIman/scripts/tmpl/*.tmpl $HOME/BLIman/scripts/
+echo "*Making a copy of the templ file"
+cp "$HOME/BLIman/scripts/tmpl/get.bliman.io.tmpl" "$HOME/BLIman/scripts"
 # replacing @xxx@ variables with acutal values.
-for file in $HOME/BLIman/scripts/*.tmpl;
-do
-    sed -i "s/@BLIMAN_VERSION@/$bli_version/g" $file
-    sed -i "s/@BLIMAN_NAMESPACE@/$BLIMAN_NAMESPACE/g" $file
-    # renaming to remove .tmpl extension
-    mv "$file" "${file//.tmpl/}"
-done
 
+echo "* Replacing @XXX@ variables with actual values"
+sed -i "s/@BLIMAN_VERSION@/$bli_version/g" "$file"
+sed -i "s/@BLIMAN_NAMESPACE@/$BLIMAN_NAMESPACE/g" "$file"
+# renaming to remove .tmpl extension
+echo "Renaming to remove .tmpl extension"
+mv "$file" "${file//.tmpl/}"
+
+mkdocs new bliman
+mv "$HOME/BLIman/bliman" "$HOME/BLIman"
+
+echo "Listing scripts"
+ls "$HOME/BLIman/scripts"
+echo "Moving to docs"
 mv "$HOME/BLIman/scripts/get.bliman.io" "$HOME/BLIMAN/docs/"
 
+
 # committing the changes
-git add $HOME/BLIman/scripts/*.* 
+git add "$HOME/BLIman/scripts/get.bliman.io"
 git commit -m "Updating version of $branch to $bli_version"
 
 #push release branch
 git push -f -u origin $branch
 
-#Push tag
-git tag -a $bli_version -m "Releasing version $bli_version"
-git push origin $bli_version
 
+
+#Push tag
+echo "Deploying mkdocs"
 mkdocs gh-deploy
+git tag -a "$bli_version" -m "Releasing version $bli_version"
+git push origin "$bli_version"
+
 
 #checkout to issue#4
 git checkout issue#4
