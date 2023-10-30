de055eb762c9b7a9ff542b5077ba0a2f47ec84bd
Arun Suresh <arun.suresh@wipro.com>
2023-10-30 15:37:31 +0530

    Updated script

--- candidates/download/host/0.0.1/windowsx64/installer.sh
+++ candidates/download/host/0.0.1/windowsx64/installer.sh
@@ -24,36 +24,22 @@ echo ' ##     ## ##     ## ##     ## ##        '
 echo ' ##     ##  #######  ########  ########  '
 echo ''
 
-if [[ ! -d $HOME/.oah ]]; then
-  	echo "Installing oah-shell"
-  	__bliman_secure_curl -s https://raw.githubusercontent.com/Be-Secure/oah-installer/master/install.sh | bash
-	source "$HOME/.oah/bin/oah-init.sh"
-else
-	echo "oah-shell found"
-fi
+# if [[ ! -d $HOME/.oah ]]; then
+#   	echo "Installing oah-shell"
+#   	__bliman_secure_curl https://raw.githubusercontent.com/Be-Secure/oah-installer/master/install.sh | bash
+# 	source "$HOME/.oah/bin/oah-init.sh"
+# else
+# 	echo "oah-shell found"
+# fi
 
-if [[ -z $(which virtualbox) ]]; then
-  	VIRTUALBOX_URL="https://download.virtualbox.org/virtualbox/6.1.16/VirtualBox-6.1.16-140961-Win.exe"
+if [[ -z $(which VBoxManage) ]]; then
 
-	# Download the VirtualBox installer to the current directory
-	wget "$VIRTUALBOX_URL"
-	
-	# Check if the download was successful
-	if [ $? -eq 0 ]; then
-	    # Run the VirtualBox installer silently
-	    powershell.exe -Command "Start-Process -Wait -FilePath .\VirtualBox-6.1.16-140961-Win.exe -ArgumentList '/S'"
-	
-	    # Check the installation result
-	    if [ $? -eq 0 ]; then
-	        echo "Oracle VM VirtualBox has been successfully installed."
-	    else
-	        echo "VirtualBox installation failed."
-	    fi
-	else
-	    echo "Download of VirtualBox installer failed. Please check your internet connection or the URL."
-	fi
+	echo "Downloading Oracle VM VirtualBox"
+	wget -P "$BLIMAN_DIR/tmp" https://download.virtualbox.org/virtualbox/7.0.12/VirtualBox-7.0.12-159484-Win.exe 
+	./"$BLIMAN_DIR"/tmp/VirtualBox-7.0.12-159484-Win.exe 
+
 else
-	echo "virtualbox found"
+	echo "VirtualBox found"
 fi
 
 # if [[ -z $(which vagrant) ]]; then
