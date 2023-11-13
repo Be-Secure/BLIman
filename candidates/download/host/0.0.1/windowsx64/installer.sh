4a8ca995f282a4f6001b720758063a59d377a864
Arun Suresh <arun.suresh@wipro.com>
2023-11-13 10:52:45 +0530

    Code updated

--- candidates/download/host/0.0.1/windowsx64/installer.sh
+++ candidates/download/host/0.0.1/windowsx64/installer.sh
@@ -34,42 +34,21 @@ fi
 
 if [[ ! -d "/c/Program Files/Oracle/VirtualBox" ]]; then
 	
-	[[ -f "$BLIMAN_DIR/tmp/virtualbox.exe" ]] && rm "$BLIMAN_DIR/tmp/virtualbox.exe"
-	echo "Downloading Oracle VM VirtualBox"
-	curl --fail --location --progress-bar --insecure https://download.virtualbox.org/virtualbox/7.0.12/VirtualBox-7.0.12-159484-Win.exe > "$BLIMAN_DIR/tmp/virtualbox.exe"
-	cd "$BLIMAN_DIR/tmp" || return 1
-	echo "Please follow the steps in installation wizard"
-	./virtualbox.exe
+	echo "VirtualBox not found".
+	echo ""
+	echo "Please install VirtualBox to continue"
 else
 	echo "VirtualBox found"
+	VBoxManage --version
 fi
 
 if [[ -z "$(command -v vagrant)" ]]; then
 	
-	echo "Downloading vagrant"
-	curl --fail --location --progress-bar --insecure https://releases.hashicorp.com/vagrant/2.4.0/vagrant_2.4.0_windows_amd64.msi > "$BLIMAN_DIR/tmp/vagrant.msi"
-	cd "$BLIMAN_DIR/tmp" || return 1
-	msiexec vagrant.msi
+	echo "Vagrant not found".
+	echo ""
+	echo "Please install Vagrant to continue"
 else
-
 	echo "vagrant found"
+	vagrant --version
 
 fi
-
-# if [[ -z $(which vagrant) ]]; then
-#   	echo "Installing vagrant"
-#   	wget https://releases.hashicorp.com/vagrant/2.2.19/vagrant_2.2.19_x86_64.deb
-# 	sudo apt install ./vagrant_2.2.19_x86_64.deb
-# else
-# 	echo "vagrant found"
-# fi
-
-# if [[ -z $(which ansible) ]]; then
-# 	echo "Installing ansible"
-# 	sudo apt update
-# 	sudo apt-add-repository --yes ppa:ansible/ansible
-# 	sudo apt update
-# 	sudo apt install ansible -y
-# else
-# 	echo "Ansible found"
-# fi
