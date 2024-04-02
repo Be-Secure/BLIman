f39b964cec2176a1b0fefb87c4b698152255c3f8
ANIL SINGLA <anilsingla111@gmail.com>
2024-04-02 22:04:09 +0530

    corrected light spelling and added development check in lite installer.sh
    If BLIMAN_IS_REL=false set than install besman using quick install rather than released version.

--- candidates/download/lite/0.0.1/linuxx64/installer.sh
+++ candidates/download/lite/0.0.1/linuxx64/installer.sh
@@ -7,13 +7,13 @@ echo ' ##     ## ##             ## ##       ######### ##     ##  '
 echo ' ##     ## ##       ##    ## ##       ##     ## ##     ##  '
 echo ' ########  ########  ######  ######## ##     ## ########   '
 echo ''
-echo ' ##       ####  ######   ##     ## ########  '
-echo ' ##        ##  ##    ##  ##     ##    ##     '
-echo ' ##        ##  ##        ##     ##    ##     '
-echo ' ##        ##  ##   #### #########    ##     '
-echo ' ##        ##  ##    ##  ##     ##    ##     '
-echo ' ##        ##  ##    ##  ##     ##    ##     '
-echo ' ######## ####  ######   ##     ##    ##     '
+echo ' ##       #### ########  ########  '
+echo ' ##        ##     ##     ##        '
+echo ' ##        ##     ##     ##        '
+echo ' ##        ##     ##     ######    '
+echo ' ##        ##     ##     ##        '
+echo ' ##        ##     ##     ##        '
+echo ' ######## ####    ##     ########  '
 echo ''
 echo ' ##     ##  #######  ########  ########  '
 echo ' ###   ### ##     ## ##     ## ##        '
@@ -25,7 +25,11 @@ echo ' ##     ##  #######  ########  ########  '
 echo ''
 if [[ ! -d $HOME/.besman ]]; then
 	echo "Installing BeSman"
-	curl -L "https://raw.githubusercontent.com/Be-Secure/BeSman/dist/dist/get.besman.io" | bash
+        if [  ! -z $BLIMAN_IS_REL ] && [ "$BLIMAN_IS_REL" == "false" ];then
+          curl -L "https://raw.githubusercontent.com/Be-Secure/BeSman/master/quick_install.sh" | bash
+        else
+	   curl -L "https://raw.githubusercontent.com/Be-Secure/BeSman/dist/dist/get.besman.io" | bash
+        fi
 	source "$HOME"/.besman/bin/besman-init.sh
 else
 	echo "BeSman found"
