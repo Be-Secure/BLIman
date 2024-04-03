e5abfb5a1328dc9fa41962b2e2eb7bc82e3d1bea
ANIL SINGLA <anilsingla111@gmail.com>
2024-04-03 10:35:55 +0530

    added echo to check if release
    added echo

--- candidates/download/lite/0.0.1/linuxx64/installer.sh
+++ candidates/download/lite/0.0.1/linuxx64/installer.sh
@@ -25,7 +25,8 @@ echo ' ##     ##  #######  ########  ########  '
 echo ''
 if [[ ! -d $HOME/.besman ]]; then
 	echo "Installing BeSman"
-        if [  ! -z $BLIMAN_IS_REL ] && [ "$BLIMAN_IS_REL" == "false" ];then
+        echo "IS Release = $BLIMAN_IS_REL"
+        if [  ! -z ${BLIMAN_IS_REL} ] && [ ${BLIMAN_IS_REL} == "false" ];then
           curl -L "https://raw.githubusercontent.com/Be-Secure/BeSman/master/quick_install.sh" | bash
         else
 	   curl -L "https://raw.githubusercontent.com/Be-Secure/BeSman/dist/dist/get.besman.io" | bash
