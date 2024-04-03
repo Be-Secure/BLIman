be5bec54c0fb56c5131e34facfda1fa014dcc0b7
ANIL SINGLA <anilsingla111@gmail.com>
2024-04-03 10:54:32 +0530

    lite installer to install using quick_install
    lite installer to install using quick_install

--- candidates/download/lite/0.0.1/linuxx64/installer.sh
+++ candidates/download/lite/0.0.1/linuxx64/installer.sh
@@ -27,7 +27,14 @@ if [[ ! -d $HOME/.besman ]]; then
 	echo "Installing BeSman"
         echo "IS Release = $BLIMAN_IS_REL"
         if [  ! -z ${BLIMAN_IS_REL} ] && [ ${BLIMAN_IS_REL} == "false" ];then
-          curl -L "https://raw.githubusercontent.com/Be-Secure/BeSman/master/quick_install.sh" | bash
+	  prd=`pwd`
+          cd /opt/
+	  git clone https://github.com/Be-Secure/BeSman.git
+          cd BeSman
+	  ./quick_install.sh
+          rm -rf /opt/BeSman
+          cd $prd
+          #curl -L "https://raw.githubusercontent.com/Be-Secure/BeSman/master/quick_install.sh" | bash
         else
 	   curl -L "https://raw.githubusercontent.com/Be-Secure/BeSman/dist/dist/get.besman.io" | bash
         fi
