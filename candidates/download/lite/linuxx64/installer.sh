2ce02466cda14c21008fb33a701f02d2c6f3862c
asingla <anilsingla111@gmail.com>
2024-04-26 18:30:38 +0530

    cleaning up in installer

--- candidates/download/lite/linuxx64/installer.sh
+++ candidates/download/lite/linuxx64/installer.sh
@@ -41,23 +41,9 @@ if [[ ! -d $HOME/.besman ]]; then
         else
 
            if [ ! -z ${BESMAN_VER} ];then
-	      __bliman_echo_yellow "Installing BeSMan version ${BESMAN_VER}"	   
-              curl --silent -o $tmp_location/besman-${BESMAN_VER}.zip --fail --location --progress-bar "https://github.com/Be-Secure/BeSMan/archive/refs/tags/${BESMAN_VER}.zip"
-              unzip -qd $tmp_location/  $tmp_location/besman-${BESMAN_VER}.zip
-	      current_wd=`pwd`
-	      cd $tmp_location/BeSman-${BESMAN_VER} 
-	      chmod +x quick_install.sh
-	      source quick_install.sh --force
-              cd $current_wd
-	      [[ -f ${BESMAN_DIR}/var/version.txt ]] && echo "${BESMAN_VER}" > "${BESMAN_DIR}/var/version.txt"
-              besman_user_config_file="${BESMAN_DIR}/etc/user-config.cfg"
-              sed -i "/BESMAN_VERSION=/c\BESMAN_VERSION=${BESMAN_VER}" $besman_user_config_file
- 
-              __bliman_echo_green "#####################################################################################"
-              __bliman_echo_green "                  Installed BeSMan version ${BESMAN_VER} successfully."
-              __bliman_echo_green "#####################################################################################"
-	   else
-               response=$(curl -s "https://api.github.com/repos/Be-Secure/BeSMan/releases/latest")
+              besver=${BESMAN_VER}
+           else
+              response=$(curl -s "https://api.github.com/repos/Be-Secure/BeSMan/releases/latest")
 
               if [[ $response == *"message"*"Not Found"* ]];then
                  __bliman_echo_red "BeSMan release version is not found."
@@ -65,24 +51,34 @@ if [[ ! -d $HOME/.besman ]]; then
                  return 1
               else
                  besver=$(echo "$response" | jq -r '.tag_name')
-              fi
-	      __bliman_echo_yellow "Installing BeSMan version ${besver}"
-              export BESMAN_VER=$besver
-	      curl --silent -o $tmp_location/besman-${besver}.zip --fail --location --progress-bar "https://github.com/Be-Secure/BeSMan/archive/refs/tags/${besver}.zip"
-              unzip -qd $tmp_location/  $tmp_location/besman-${besver}.zip
-              current_wd=`pwd`
-              cd $tmp_location/BeSman-${besver}
-              chmod +x quick_install.sh
-              source quick_install.sh --force
-              cd $current_wd
-	      [[ -f ${BESMAN_DIR}/var/version.txt ]] && echo "${besver}" > "${BESMAN_DIR}/var/version.txt"
-	      besman_user_config_file="${BESMAN_DIR}/etc/user-config.cfg"
-	      sed -i "/BESMAN_VERSION=/c\BESMAN_VERSION=${besver}" $besman_user_config_file
-              __bliman_echo_green "#####################################################################################"
-	      __bliman_echo_green "                  Installed BeSMan version ${besver} successfully."
-	      __bliman_echo_green "#####################################################################################"
-           fi
+              fi 
+	   fi
+           __bliman_echo_yellow "Installing BeSMan version ${besver}"
+           export BESMAN_VER=$besver
+           curl --silent -o $tmp_location/besman-${besver}.zip --fail --location --progress-bar "https://github.com/Be-Secure/BeSMan/archive/refs/tags/${besver}.zip"
+           unzip -qd $tmp_location/  $tmp_location/besman-${besver}.zip
+           current_wd=`pwd`
+           cd $tmp_location/BeSman-${besver}
+           chmod +x quick_install.sh
+           source quick_install.sh --force
+           cd $current_wd
+           [[ -f ${BESMAN_DIR}/var/version.txt ]] && echo "${besver}" > "${BESMAN_DIR}/var/version.txt"
+           besman_user_config_file="${BESMAN_DIR}/etc/user-config.cfg"
+           sed -i "/BESMAN_VERSION=/c\BESMAN_VERSION=${besver}" $besman_user_config_file
         fi
+
+	if [ -f "$HOME/.besman/bin/besman-init.sh" ];then
+                __bliman_echo_green "#####################################################################################"
+                __bliman_echo_green "                  Installed BeSMan version ${BESMAN_VER} successfully."
+                __bliman_echo_green "#####################################################################################"
+        else
+                #__bliman_echo_red "#####################################################################################"
+                __bliman_echo_red " >>>>>>>>>> BeSMan version ${BESMAN_VER} NOT installed properly."
+                #__bliman_echo_red " >>>>>>>>>> BeSMan version ${BESMAN_VER} NOT installed properly."
+                #__bliman_echo_red "#####################################################################################"
+		return 1
+        fi
+
 	if [  -f "$HOME/.besman/bin/besman-init.sh" ];then
 	   source "$HOME/.besman/bin/besman-init.sh" 2>&1 | __bliman_log
 	else
