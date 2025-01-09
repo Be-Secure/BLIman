19ee91e990a981265302c29d1b5d44cac7160128
asa1997 <arun.suresh@wipro.com>
2025-01-09 12:10:52 +0530

    Added plugin argument

--- src/main/bash/bliman-info.sh
+++ src/main/bash/bliman-info.sh
@@ -3,24 +3,30 @@
 __bli_info() {
 
     # bli info plugin <plugin name> <version>
-    local plugin_name=$1
-    local plugin_version=$2
-    local installed_plugin=false
-    
-    if [[ -d $BLIMAN_PLUGINS_DIR/$plugin_name ]] 
-    then
-        installed_plugin=true
-        source "$BLIMAN_PLUGINS_DIR/$plugin_name/$plugin_version/beslab-$plugin_name-$plugin_version-plugin.sh"
-    else
-        __bliman_get_remote_plugin "$plugin_name" "$plugin_version"
-    fi
+    local type=$1
+    local plugin_name=$2
+    local plugin_version=$3
 
-    __beslab_plugininfo_"$plugin_name"
-    if [[ "$installed_plugin" == "true" ]]
+    if [[ ( -n $type ) && ( $type == "plugin" ) ]]
     then
-        __bliman_echo_yellow "Plugin $plugin_name $plugin_version is installed in your system."
-    else
-        __bliman_echo_yellow "Plugin $plugin_name $plugin_version is available to install."
+        
+        local installed_plugin=false
+        
+        if [[ -d $BLIMAN_PLUGINS_DIR/$plugin_name ]] 
+        then
+            installed_plugin=true
+            source "$BLIMAN_PLUGINS_DIR/$plugin_name/$plugin_version/beslab-$plugin_name-$plugin_version-plugin.sh"
+        else
+            __bliman_get_remote_plugin "$plugin_name" "$plugin_version"
+        fi
+
+        __beslab_plugininfo_"$plugin_name"
+        if [[ "$installed_plugin" == "true" ]]
+        then
+            __bliman_echo_yellow "Plugin $plugin_name $plugin_version is installed in your system."
+        else
+            __bliman_echo_yellow "Plugin $plugin_name $plugin_version is available to install."
+        fi
     fi
 }
 
